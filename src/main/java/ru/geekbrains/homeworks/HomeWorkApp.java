b0bc1d905e09ef18d2a4f0fd69a15f9e78a1d43c
arseniy <arseniy.karmanov@mail.ru>
2021-10-15 00:48:51 +0700

    Homework#2: Task#1

--- src/main/java/ru/geekbrains/homeworks/HomeWorkApp.java
+++ src/main/java/ru/geekbrains/homeworks/HomeWorkApp.java
@@ -9,6 +9,12 @@ public class HomeWorkApp {
     private static final Scanner SC = new Scanner(System.in);
 
     public static void main(String[] args) {
+        System.out.println("checkSum(10, 20) = " + checkSum(10, 20));
+        System.out.println("checkSum(5, 10) = " + checkSum(5, 10));
+    }
 
+    public static boolean checkSum(int firstTerm, int secondTerm) {
+        int sum = firstTerm + secondTerm;
+        return (sum >= 10) && (sum <= 20);
     }
 }
