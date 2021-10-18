5fc0a67e55469fdb3f4615062088fa428813d1b1
arseniy <arseniy.karmanov@mail.ru>
2021-10-19 00:49:53 +0700

    Homework#3: Task#3 done

--- src/main/java/ru/geekbrains/homeworks/Homework3.java
+++ src/main/java/ru/geekbrains/homeworks/Homework3.java
@@ -1,4 +1,15 @@
 package ru.geekbrains.homeworks;
 
+import java.util.Arrays;
+
 public class Homework3 {
+    public static void main(String[] args) {
+        int[] mas = {1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0};
+
+        System.out.println(Arrays.toString(mas));
+        for (int i = 0; i < mas.length; i++)
+            if (mas[i] == 1) mas[i] = 0;
+            else mas[i] = 1;
+        System.out.println(Arrays.toString(mas));
+    }
 }
