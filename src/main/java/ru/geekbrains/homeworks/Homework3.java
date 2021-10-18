e8483192edc1b846ad35a74252de3bf44731e55e
arseniy <arseniy.karmanov@mail.ru>
2021-10-19 00:54:09 +0700

    Homework#3: Task#2 done

--- src/main/java/ru/geekbrains/homeworks/Homework3.java
+++ src/main/java/ru/geekbrains/homeworks/Homework3.java
@@ -4,12 +4,8 @@ import java.util.Arrays;
 
 public class Homework3 {
     public static void main(String[] args) {
-        int[] mas = {1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0};
-
-        System.out.println(Arrays.toString(mas));
-        for (int i = 0; i < mas.length; i++)
-            if (mas[i] == 1) mas[i] = 0;
-            else mas[i] = 1;
+        int[] mas = new int[100];
+        for (int i = 0; i < mas.length; i++) mas[i] = i + 1;
         System.out.println(Arrays.toString(mas));
     }
 }
