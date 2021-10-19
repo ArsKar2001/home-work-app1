9aad54da79c6293985864549641da18ceb700cd4
arseniy <arseniy.karmanov@mail.ru>
2021-10-19 14:06:37 +0700

    Homework#3: Task#7 done

--- src/main/java/ru/geekbrains/homeworks/Homework3.java
+++ src/main/java/ru/geekbrains/homeworks/Homework3.java
@@ -1,27 +1,41 @@
 package ru.geekbrains.homeworks;
 
-import java.util.Arrays;
-import java.util.concurrent.ThreadLocalRandom;
-
 public class Homework3 {
     public static void main(String[] args) {
-        ThreadLocalRandom rnd = ThreadLocalRandom.current();
+        int[] arr1 = {2, 2, 2, 1, 2, 2, 10, 1};
+        int[] arr2 = {1, 1, 1, 2, 1};
+        int[] arr3 = {1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2};
+        int[] arr4 = {4, 5, 6, 7, 2, 34, 2, 3};
 
-        int len = rnd.nextInt(7) + 3;
-        int[] arr = new int[len];
+        System.out.println("checkBalance(arr1) = " + checkBalance(arr1));
+        System.out.println("checkBalance(arr2) = " + checkBalance(arr2));
+        System.out.println("checkBalance(arr3) = " + checkBalance(arr3));
+        System.out.println("checkBalance(arr4) = " + checkBalance(arr4));
+    }
 
-        for (int i = 0; i < arr.length; i++)
-            arr[i] = rnd.nextInt(10);
-
-        int minValue = arr[0];
-        int maxValue = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            if (minValue > arr[i]) minValue = arr[i];
-            if (maxValue < arr[i]) maxValue = arr[i];
+    public static boolean checkBalance(int[] arr) {
+        if (arr.length > 0) {
+            int leftSum = arr[0];
+            int rightSum = arr[arr.length - 1];
+            for (int i = 1, j = arr.length - 2; i < arr.length || j >= 0; ) {
+                if (leftSum < rightSum) {
+                    leftSum += arr[i];
+                    i++;
+                    continue;
+                }
+                if (leftSum > rightSum) {
+                    rightSum += arr[j];
+                    j--;
+                    continue;
+                }
+                if (Math.abs(i - j) != 1) {
+                    leftSum += arr[i];
+                    rightSum += arr[j];
+                    i++;
+                    j--;
+                } else return true;
+            }
         }
-
-        System.out.println(Arrays.toString(arr));
-        System.out.println("maxValue = " + maxValue);
-        System.out.println("minValue = " + minValue);
+        return false;
     }
 }
