407df5f10b7bfd2ab915bf3e1e9289ad72c1bd0d
arseniy <arseniy.karmanov@mail.ru>
2021-10-19 14:35:42 +0700

    Homework#3: Task#8 done

--- src/main/java/ru/geekbrains/homeworks/Homework3.java
+++ src/main/java/ru/geekbrains/homeworks/Homework3.java
@@ -1,41 +1,35 @@
 package ru.geekbrains.homeworks;
 
+import java.util.Arrays;
+
 public class Homework3 {
     public static void main(String[] args) {
-        int[] arr1 = {2, 2, 2, 1, 2, 2, 10, 1};
-        int[] arr2 = {1, 1, 1, 2, 1};
-        int[] arr3 = {1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2};
-        int[] arr4 = {4, 5, 6, 7, 2, 34, 2, 3};
-
-        System.out.println("checkBalance(arr1) = " + checkBalance(arr1));
-        System.out.println("checkBalance(arr2) = " + checkBalance(arr2));
-        System.out.println("checkBalance(arr3) = " + checkBalance(arr3));
-        System.out.println("checkBalance(arr4) = " + checkBalance(arr4));
+        int[] arr1 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] arr2 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        System.out.println(Arrays.toString(arr1));
+        System.out.println(Arrays.toString(shiftArrayElements(arr1, 5)));
+        System.out.println(Arrays.toString(shiftArrayElements(arr2, -5)));
     }
 
-    public static boolean checkBalance(int[] arr) {
-        if (arr.length > 0) {
-            int leftSum = arr[0];
-            int rightSum = arr[arr.length - 1];
-            for (int i = 1, j = arr.length - 2; i < arr.length || j >= 0; ) {
-                if (leftSum < rightSum) {
-                    leftSum += arr[i];
-                    i++;
-                    continue;
+    public static int[] shiftArrayElements(int[] array, int n) {
+        int valOld;
+        while (Math.abs(n) != 0) {
+            if (n < 0) {
+                for (int i = 0; i < array.length - 1; i++) {
+                    valOld = array[i + 1];
+                    array[i + 1] = array[i];
+                    array[i] = valOld;
                 }
-                if (leftSum > rightSum) {
-                    rightSum += arr[j];
-                    j--;
-                    continue;
+                n++;
+            } else {
+                for (int i = array.length - 1; i > 0; i--) {
+                    valOld = array[i - 1];
+                    array[i - 1] = array[i];
+                    array[i] = valOld;
                 }
-                if (Math.abs(i - j) != 1) {
-                    leftSum += arr[i];
-                    rightSum += arr[j];
-                    i++;
-                    j--;
-                } else return true;
+                n--;
             }
         }
-        return false;
+        return array;
     }
 }
